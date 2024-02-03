cbffa0e58972c98409304a191236be93ff63ceee
Go-Adler <gokul_adler@outlook.com>
2024-02-03 12:50:27 +0530

    initialised frontend

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component'
+
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'auth'
+  },
+  { path: 'auth',
+    component: AuthComponent
+  }
+];
